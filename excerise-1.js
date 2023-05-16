1caacc176fa1b43ea3e43d262223440855ae49b5
Deepak chan <deepakchan@Deepaks-MacBook-Pro.local>
2023-05-16 19:39:16 -0400

    contains duplicate

---
+++ excerise-1.js
@@ -0,0 +1,13 @@
+ var containsDuplicate = function(nums) {
+   
+    for (let i = 0; i < nums.length; i++) {
+        
+         for (let k=i+1; k < nums.length; k++){
+             if(nums[i]===nums[k])
+             return true;
+             
+         }
+ 
+}
+   return false; }
+    
